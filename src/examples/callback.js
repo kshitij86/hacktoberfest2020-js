e3091e94198724e71a6c9f7025ed6c4ef90ce406
ishan chaudhary <ishan4220@gmail.com>
2020-10-04 01:47:17 +0530

    Added_Callback_function

---
+++ src/examples/callback.js
@@ -0,0 +1,15 @@
+// A callback function is a function passed into another function
+// as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+
+function greeting(name) {
+    console.log(`Hello ${name}, welcome to Hacktoberfest!`);
+}
+
+function introduction(firstName, lastName, callback) {
+    const fullName = `${firstName} ${lastName}`;
+
+    callback(fullName);
+}
+
+introduction('Chris', 'Nwamba', greeting);
+
