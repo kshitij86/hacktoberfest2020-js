3019fd17944c5abbe9c1a612af75d7fe6effdcd6
Infrasonic Lee <junyongxlee@gmail.com>
2020-10-04 00:01:06 +0800

    Added Local Global Scopes

---
+++ src/global_local_scopes.js
@@ -0,0 +1,26 @@
+//LOCAL VARIABLES
+//---------------
+//Variables declared within a JavaScript function, become LOCAL to the function.
+//Local variables have Function scope: They can only be accessed from within the function.
+
+function myFunction() {
+  var food = "Apple";
+  console.log(food); // code here CAN use food variable
+}
+console.log(food); //code here CANNOT use food variable
+
+
+
+
+//GLOBAL VARIABLES
+//---------------
+//A variable declared outside a function, becomes GLOBAL.
+//A global variable has global scope: All scripts and functions on a web page can access it. 
+
+var food = "Apple";
+
+console.log(food); // code here CAN use food variable
+
+function myFunction() {
+  console.log(food); // code here CAN use food variable
+}
