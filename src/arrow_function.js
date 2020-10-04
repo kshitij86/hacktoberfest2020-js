212d7b1a77a119fcf44be92bea6b7bbfe19f28d6
Apeiron <megha.malviya444@gmail.com>
2020-10-04 23:56:09 +0530

    arrow function example with explanation

---
+++ src/arrow_function.js
@@ -0,0 +1,14 @@
+/*
+"Arrow functions" were introduced with ES6 as a new syntax for writing JavaScript functions.
+They provide us with an alternative way to write a shorter syntax as compared to the regular function.
+Thus we can say that Arrow Functions save a developer’s time and simplify code development.
+Points to understand:
+No function keyword 
+No function name
+A list of arguments within parenthesis, followed by a 'fat arrow' (=>), followed by a function body.
+*/
+
+
+let add = (a, b) => a + b;
+
+console.log(`Sum is ${add(5,10)}`);
